3703f46a46dd677caada34fe88f4e976958d1bae
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-12-05 19:19:16 +0000

    adjusted tracker collection, drinks models

--- app/models/collection_tracker.py
+++ app/models/collection_tracker.py
@@ -1,31 +1,48 @@
 import sqlalchemy as sa
-from sqlalchemy import Boolean, Column, Integer, String, Float,
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 from sqlalchemy.sql.sqltypes import TIMESTAMP, DateTime
 from sqlalchemy.sql.expression import text
 # from app.schemas import UserInDB
+from typing import List
+
+from app.models.drink import Drink
+
 
 from app.db.base_class import Base
 
 #TRACKER/COLLECTION MODEL - PARENT
 class Collection(Base):
-    __tablename__ = "collection_tracker"
+    __tablename__ = "collection_trackers"
 
-    id = Column(Integer, primary_key=True, index=True)
-# foreign key connecter to business
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+#? foreign key connecter to business or is this only in relationship?
     user_id = Column(Integer, foreign_key=True, index=True)
-    title = Column(String, index=True)
+
+    title: Mapped[str] = Column(String, default="Name" index=True)
 # where a list image would be added ---> logo_pic = Column()
-    ordered_list_number = Column(Integer, nullable=True)
-    contact_info = Column(String, nullable=True)
-    website_url = Column(String, nullable=True)
-    instagram_handle = Column(String, nullable=True)
-    event_promotion_time_start = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
-    event_promotion_time_stop = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
-    event_promotion_banner = Column(String, nullable=True)
-    favorite = Column(Boolean(), nullable=False) #compare to is superuser?
-    created_timestamp = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    updated_timestamp = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
+    ordered_list_number: Mapped[int] = Column(Integer, nullable=True)
+    contact_info: Mapped[str] = Column(String, default="Contact Info", nullable=True)
+    website_url: Mapped[str] = Column(String, default="Website URL" nullable=True)
+    instagram_handle: Mapped[str] = Column(String, default="Insta Handle", nullable=True)
+    event_promotion_time_start: Mapped[str] = Column(TIMESTAMP(timezone=True), default="Event Start Date", nullable=True, server_default=text('now()'))
+    event_promotion_time_stop: Mapped[str] = Column(TIMESTAMP(timezone=True), default="Event End Date", nullable=True, server_default=text('now()'))
+    event_promotion_banner: Mapped[str] = Column(String, nullable=True)
+    favorite: Mapped[bool] = Column(Boolean(), nullable=False) #compare to is superuser?
+    created_timestamp: Mapped[str] = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    updated_timestamp: Mapped[str] = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
 
 # relationships
-    relationship()
+    drinks: Mapped[List["CollectionTrackerDrink"]] = relationship(back_populates="collection_tracker")
+
+#! pivot/child Collection drinks
+class CollectionTrackerDrink (Base):
+    __tablename__ = "collection_tracker_drinks"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    collection_tracker_id: Mapped[int] = mapped_column(ForeignKey("collection_tracker.id"))
+    drink_id: Mapped[int] = mapped_column(ForeignKey("drink.id"))
+
+# relationship
+    collection_tracker: Mapped["Collection"] = relationship(back_populates="drinks")
+    drink: Mapped["Drink"] = relationship(back_populates="collection_trackers")
+    #tag has businesses relationship
--- app/models/drink.py
+++ app/models/drink.py
@@ -1,9 +1,12 @@
 import sqlalchemy as sa
-from sqlalchemy import Boolean, Column, Integer, String, Float,
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 from sqlalchemy.sql.sqltypes import TIMESTAMP, DateTime
 from sqlalchemy.sql.expression import text
 # from app.schemas import UserInDB
+from typing import List
+
+from app.models.collection_tracker import CollectionTrackerDrink
 
 from app.db.base_class import Base
 
@@ -11,17 +14,18 @@ from app.db.base_class import Base
 class Drink(Base):
     __tablename__ = "drinks"
 
-    id = Column(Integer, primary_key=True, index=True)
-# foreign key connecter to business
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+#? do i connect foreign key connecter to business or is that only in relationship?
     business_id = Column(Integer, foreign_key=True, index=True)
-    name = Column(String, index=True)
+
+    name: Mapped[str] = Column(String, default="Name", index=True)
 # where a drink image would be added ---> logo_pic = Column()
-    description = Column(String, nullable=True)
-    price = Column(Float, nullable=False)
-    featured = Column(Boolean(), nullable=False) #compare to is superuser?
-    ordered_list_number = Column(Integer, nullable=True)
-    created_timestamp = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    updated_timestamp = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
+    description: Mapped[str] = Column(String, default="Description", nullable=True)
+    price: Mapped[float] = Column(Float, default="Price", nullable=False)
+    featured: Mapped[bool] = Column(Boolean(), default="Featured", nullable=False) #compare to is superuser?
+    ordered_list_number: Mapped[str] = Column(Integer, nullable=True)
+    created_timestamp: Mapped[str] = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    updated_timestamp: Mapped[str] = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
 
 # relationships
-    relationship()
+    collection_trackers: Mapped[List["CollectionTrackerDrink"]] = relationship(back_populates="drink")
